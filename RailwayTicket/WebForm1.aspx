d13b3d994b0d119a4fa20b3a374f3d405f6bf75c
rajnish1242 <132262374+rajnish1242@users.noreply.github.com>
2023-05-01 15:35:59 +0530

    Add files via upload

---
+++ RailwayTicket/WebForm1.aspx
@@ -0,0 +1,242 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="RailwayTicket.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 129px;
+        }
+        .auto-style3 {
+            width: 127px;
+        }
+        .auto-style4 {
+            width: 128px;
+        }
+        .auto-style5 {
+            width: 49px;
+        }
+        .auto-style6 {
+            width: 129px;
+            margin-left: 200px;
+        }
+        .auto-style7 {
+            width: 129px;
+            height: 23px;
+        }
+        .auto-style8 {
+            width: 127px;
+            height: 23px;
+        }
+        .auto-style9 {
+            width: 128px;
+            height: 23px;
+        }
+        .auto-style10 {
+            width: 49px;
+            height: 23px;
+        }
+        .auto-style11 {
+            height: 23px;
+        }
+        .auto-style12 {
+            width: 649px;
+        }
+        .auto-style13 {
+            width: 649px;
+            height: 23px;
+        }
+        .auto-style14 {
+            width: 63px;
+        }
+        .auto-style15 {
+            height: 23px;
+            width: 63px;
+        }
+        </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td class="auto-style12">
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; TICKET&nbsp; BOOKING</td>
+                <td class="auto-style3">
+                    &nbsp;</td>
+                <td class="auto-style4">&nbsp;</td>
+                <td class="auto-style5">&nbsp;</td>
+                <td class="auto-style14">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td class="auto-style12">
+                    &nbsp;</td>
+                <td class="auto-style3">
+                    &nbsp;</td>
+                <td class="auto-style4">&nbsp;</td>
+                <td class="auto-style5">&nbsp;</td>
+                <td class="auto-style14">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td class="auto-style12">
+                    &nbsp;</td>
+                <td class="auto-style3">
+                    &nbsp;</td>
+                <td class="auto-style4">&nbsp;</td>
+                <td class="auto-style5">&nbsp;</td>
+                <td class="auto-style14">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td class="auto-style12">
+                    <asp:Button ID="Button2" runat="server" Text="new" OnClick="Button2_Click" />
+                </td>
+                <td class="auto-style3">
+                    <asp:Button ID="Button3" runat="server" Text="show" OnClick="Button3_Click" />
+                </td>
+                <td class="auto-style4">&nbsp;</td>
+                <td class="auto-style5">&nbsp;</td>
+                <td class="auto-style14">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style6">
+                    &nbsp;PNR No.&nbsp;</td>
+                <td class="auto-style12">
+                    D.O.J.&nbsp;</td>
+                <td class="auto-style3">
+                    From</td>
+                <td class="auto-style4">
+                    To&nbsp;</td>
+                <td class="auto-style5">&nbsp;</td>
+                <td class="auto-style14">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style6">
+                    <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged" ReadOnly="True"></asp:TextBox>
+
+                </td>
+                <td class="auto-style12">
+                    <asp:TextBox ID="TextBox2" runat="server" TextMode="Date" AutoPostBack="True" OnTextChanged="TextBox2_TextChanged" ReadOnly="True"></asp:TextBox>
+                </td>
+                <td class="auto-style3">
+                    <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" Enabled="False">
+                    </asp:DropDownList>
+                </td>
+                <td class="auto-style4">
+                    <asp:DropDownList ID="DropDownList2" runat="server" Enabled="False" OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged">
+                    </asp:DropDownList>
+                </td>
+                <td class="auto-style5">&nbsp;</td>
+                <td class="auto-style14">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style7"></td>
+                <td class="auto-style13"></td>
+                <td class="auto-style8"></td>
+                <td class="auto-style9"></td>
+                <td class="auto-style10"></td>
+                <td class="auto-style15"></td>
+                <td class="auto-style11"></td>
+            </tr>
+            <tr>
+                <td class="auto-style7">Name</td>
+                <td class="auto-style13">DOB</td>
+                <td class="auto-style8">Gender</td>
+                <td class="auto-style9">Seat Type</td>
+                <td class="auto-style10">&nbsp;</td>
+                <td class="auto-style15">&nbsp;</td>
+                <td class="auto-style11">&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:TextBox ID="TextBox5" runat="server" OnTextChanged="TextBox5_TextChanged" Visible="False"></asp:TextBox>
+                </td>
+                <td class="auto-style12">
+                    <asp:TextBox ID="TextBox6" runat="server" OnTextChanged="TextBox6_TextChanged" TextMode="Date" Visible="False"></asp:TextBox>
+
+
+                    <br />
+
+
+                </td>
+                <td class="auto-style3">
+                    <asp:RadioButtonList ID="RadioButtonList1" runat="server" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged" Visible="False">
+                        <asp:ListItem>MALE</asp:ListItem>
+                        <asp:ListItem>FEMALE</asp:ListItem>
+                        <asp:ListItem>OTHERS</asp:ListItem>
+                    </asp:RadioButtonList>
+                </td>
+                <td class="auto-style4">
+                    <asp:DropDownList ID="DropDownList3" runat="server" OnSelectedIndexChanged="DropDownList3_SelectedIndexChanged" Visible="False">
+                    </asp:DropDownList>
+                </td>
+                <td class="auto-style5">
+                    <asp:Button ID="Button1" runat="server" Text="Save" OnClick="Button1_Click" />
+                </td>
+                <td class="auto-style14">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td class="auto-style12">&nbsp;</td>
+                <td class="auto-style3">&nbsp;</td>
+                <td class="auto-style4">&nbsp;</td>
+                <td class="auto-style5">&nbsp;</td>
+                <td class="auto-style14">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2">&nbsp;</td>
+                <td class="auto-style12" id="form1">
+                    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating" OnRowDeleting="GridView1_RowDeleting" OnRowCancelingEdit="GridView1_RowCancelingEdit" OnSelectedIndexChanged="GridView1_SelectedIndexChanged1" Height="160px" Width="255px">
+      <Columns>
+               <asp:BoundField HeaderText="ID" DataField="Id" ReadOnly="true"/>
+               <asp:BoundField HeaderText="PNR No." DataField="PNR" ReadOnly="true"/>
+               <asp:BoundField HeaderText="Date Of Journey" DataField="DateOfJourney"/>
+               <asp:BoundField HeaderText="Boarding City" DataField="Boarding" />
+               <asp:BoundField HeaderText="Destination City" DataField="Destination"/>
+               <asp:BoundField HeaderText="Passenger Name" DataField="Name" ReadOnly="true"/>
+               <asp:BoundField HeaderText="DOB" DataField="DOB"/>
+               <asp:BoundField HeaderText="Passenger Gender" DataField="Gender"/>
+               <asp:BoundField HeaderText="Seat Type" DataField="SeatType"/>
+               <asp:CommandField ShowEditButton="true" ShowDeleteButton="true"/>
+      </Columns>
+  </asp:GridView>
+
+                </td>
+                <td class="auto-style3">
+                    &nbsp;</td>
+                <td class="auto-style4">
+                    &nbsp;</td>
+                <td class="auto-style5">&nbsp;</td>
+                <td class="auto-style14">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style7"></td>
+                <td class="auto-style13"></td>
+                <td class="auto-style8"></td>
+                <td class="auto-style9"></td>
+                <td class="auto-style10"></td>
+                <td class="auto-style15"></td>
+                <td class="auto-style11"></td>
+            </tr>
+        </table>
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ RailwayTicket/WebForm1.aspx.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace RailwayTicket
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string c = ConfigurationManager.ConnectionStrings["sqlconnection"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string q = "select * from city";
+                SqlConnection cn = new SqlConnection(c);
+                cn.Open();
+                SqlCommand cmd = new SqlCommand(q, cn);
+                DropDownList1.DataSource = cmd.ExecuteReader();
+                DropDownList1.DataTextField = "name";
+                DropDownList1.DataValueField = "id";
+                DropDownList1.DataBind();
+                DropDownList1.Items.Insert(0, new ListItem("---Select City---", "0"));
+                cn.Close();
+
+                string q1 = "select * from city";
+                SqlConnection cn1 = new SqlConnection(c);
+                cn1.Open();
+                SqlCommand cmd1 = new SqlCommand(q1, cn1);
+                DropDownList2.DataSource = cmd1.ExecuteReader();
+                DropDownList2.DataTextField = "name";
+                DropDownList2.DataValueField = "id";
+                DropDownList2.DataBind();
+                DropDownList2.Items.Insert(0, new ListItem("---Select City---", "0"));
+                cn1.Close();
+
+                string q2 = "select * from CLASS";
+                SqlConnection cn2 = new SqlConnection(c);
+                cn2.Open();
+                SqlCommand cmd2 = new SqlCommand(q2, cn2);
+                DropDownList3.DataSource = cmd2.ExecuteReader();
+                DropDownList3.DataTextField = "CLASS";
+                DropDownList3.DataValueField = "id";
+                DropDownList3.DataBind();
+                DropDownList3.Items.Insert(0, new ListItem("---Select Class---", "0"));
+                cn2.Close();
+            }
+
+        }
+
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (Convert.ToDateTime(TextBox2.Text) < DateTime.Now)
+            {
+                Response.Write("<script language='javascript'>alert(' please select correct date ');</script>");
+                Response.Write("---hklkl---");
+            }
+        }
+
+        protected void TextBox6_TextChanged(object sender, EventArgs e)
+        {
+            if (Convert.ToDateTime(TextBox6.Text) > DateTime.Now)
+                Response.Write("<script language='javascript'>alert(' please select correct date ');</script>");
+        }
+       
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public string CreatePassword(int length)
+        {
+            const string valid = "1234567890";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--)
+            {
+                res.Append(valid[rnd.Next(valid.Length)]);
+            }
+            return res.ToString();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            TextBox1.ReadOnly =false;
+            TextBox2.ReadOnly = false;
+            DropDownList1.Enabled = true;
+            DropDownList2.Enabled = true;
+            TextBox1.Text = CreatePassword(7);
+
+
+        }
+
+      
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            TextBox5.Visible = true;
+            TextBox6.Visible = true;
+            RadioButtonList1.Visible = true;
+            DropDownList3.Visible = true;
+
+
+            string s = "select Details.Id,Details.PNR,Details.DateOfJourney,Details.Boarding,Details.Destination,Details1.Name,Details1.DOB,Details1.Gender,Details1.SeatType from Details inner join Details1 on Details1.Id = details.Id where PNR = '" + TextBox1.Text + "'";
+            SqlConnection cn = new SqlConnection(c);
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(s, cn);
+            da.Fill(ds);
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+
+
+        }
+
+        protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private string id;
+        static int count = 0;
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            count = count + 1;
+            if (TextBox1.Text != "" && TextBox2.Text != "" && TextBox5.Text != "" && TextBox6.Text != ""
+             && DropDownList1.SelectedValue != "0" && DropDownList2.SelectedValue != "0"
+             && DropDownList3.SelectedValue != "0" && RadioButtonList1.SelectedValue != "0" &&
+             DropDownList1.SelectedItem.Text != DropDownList2.SelectedItem.Text && count <= 5)
+            {
+
+                if (count == 1)
+                {
+                    string q = "insert into details values('" + TextBox1.Text + "', '" + TextBox2.Text + "', '" + DropDownList1.SelectedItem.Text + "','" + DropDownList2.SelectedItem.Text + "')";
+                    SqlConnection cn = new SqlConnection(c);
+                    cn.Open();
+                    SqlCommand cmd = new SqlCommand(q, cn);
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                }
+                string q1 = "select * from details where PNR ='" + TextBox1.Text + "'";
+                SqlConnection cn1 = new SqlConnection(c);
+                cn1.Open();
+                SqlCommand cmd1 = new SqlCommand(q1, cn1);
+                SqlDataReader dr = cmd1.ExecuteReader();
+                while (dr.Read())
+                {
+                    id = dr["Id"].ToString();
+                }
+                cn1.Close();
+
+                string q2 = "insert into details1 values('" + TextBox5.Text + "', '" + TextBox6.Text + "','" + RadioButtonList1.SelectedItem.Text + "','" + DropDownList3.SelectedItem.Text + "','" + id + "')";
+                SqlConnection cn2 = new SqlConnection(c);
+                cn2.Open();
+                SqlCommand cmd2 = new SqlCommand(q2, cn2);
+                int k2 = cmd2.ExecuteNonQuery();
+                if (k2 > 0)
+                    Response.Write("<script language='javascript'>alert('Your ticket has been booked.');</script>");
+                cn2.Close();
+            }
+            else
+            {
+                Response.Write("<script language='javascript'>alert('Please fill all fields." +
+                    " Destination should be different from boarding station." +
+                    " If you have booked 5 tickets on this PNR,please click on new button');</script>");
+
+                TextBox2.Text = string.Empty;
+                TextBox2.ReadOnly = true;
+                DropDownList1.SelectedValue = "0";
+                DropDownList1.Enabled = false;
+                DropDownList2.SelectedValue = "0";
+                DropDownList2.Enabled = false;
+                TextBox5.Text = string.Empty;
+                TextBox5.ReadOnly = true;
+                TextBox6.Text = string.Empty;
+                TextBox6.ReadOnly = true;
+                RadioButtonList1.Enabled = false;
+                DropDownList3.SelectedValue = "0";
+                DropDownList3.Enabled = false;
+                Button3.Enabled = false;
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        protected void TextBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            {
+                GridView1.EditIndex = e.NewEditIndex;
+                Button3_Click(sender, e);
+            }
+
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            string id = GridView1.Rows[e.RowIndex].Cells[0].Text;
+
+            TextBox b = (TextBox)GridView1.Rows[e.RowIndex].Cells[2].Controls[0];
+            TextBox j = (TextBox)GridView1.Rows[e.RowIndex].Cells[3].Controls[0];
+            TextBox d = (TextBox)GridView1.Rows[e.RowIndex].Cells[4].Controls[0];
+            string f = GridView1.Rows[e.RowIndex].Cells[5].Text;
+            TextBox g = (TextBox)GridView1.Rows[e.RowIndex].Cells[6].Controls[0];
+            TextBox h = (TextBox)GridView1.Rows[e.RowIndex].Cells[7].Controls[0];
+            TextBox i = (TextBox)GridView1.Rows[e.RowIndex].Cells[8].Controls[0];
+
+            string up = "update ticket_detail set dob='" + g.Text + "',gender='" + h.Text + "',seattype='" + i.Text + "' where name= '" + f + "'" +
+                "update details set DateOfJourney='" + b.Text + "',boarding='" + j.Text + "',destination='" + d.Text + "' where id= '" + id + "'";
+
+            SqlConnection k = new SqlConnection(c);
+            k.Open();
+            SqlCommand cmd = new SqlCommand(up, k);
+            cmd.ExecuteNonQuery();
+            GridView1.EditIndex = -1;
+            k.Close();
+            Button2_Click(sender, e);
+
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            string id = GridView1.Rows[e.RowIndex].Cells[0].Text;
+            string f = GridView1.Rows[e.RowIndex].Cells[5].Text;
+            string up = "delete from details1  where name='" + f + "'" +
+                        "delete from details  where id='" + id + "'";
+            SqlConnection k = new SqlConnection(c);
+            k.Open();
+            SqlCommand cmd = new SqlCommand(up, k);
+            cmd.ExecuteNonQuery();
+            k.Close();
+            Button3_Click(sender, e);
+        }
+
+        protected void GridView1_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            Button3_Click(sender, e);
+        }
+    }
+    }
